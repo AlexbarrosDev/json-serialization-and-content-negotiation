46bb0e357390d3bbe947ee31fe875c83471ec73b
Alex Barros <alexbarros.dev@gmail.com>
2026-01-20 17:08:09 -0300

    first commit

---
+++ src/main/java/com/alexdev/StartupApplication.java
@@ -0,0 +1,13 @@
+package com.alexdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupApplication.class, args);
+	}
+
+}
